bcabd33a30bb9698cd16449f783bf3d5c73aba12
iamgs7 <gs7.civic@gmail.com>
2016-05-06 03:37:42 -0700

    Initial Commit
    --Added _twitter_data_stream.py
    Streams data from Twitter using Twitter's Streaming API
    --Added _tags_count.py
    Counts the number of tweets from different locations and saves the data
    in .csv format

---
+++ _twitter_data_stream.py
@@ -0,0 +1,51 @@
+import oauth2 as oauth
+import urllib2 as urllib
+from config import *
+
+_debug = 0
+
+oauth_token = oauth.Token(key=access_token_key, secret=access_token_secret)
+oauth_consumer = oauth.Consumer(key=api_key, secret=api_secret)
+
+signature_method_hmac_sha1 = oauth.SignatureMethod_HMAC_SHA1()
+
+http_method = "POST"
+
+http_handler = urllib.HTTPHandler(debuglevel=_debug)
+https_handler = urllib.HTTPSHandler(debuglevel=_debug)
+
+
+def twitterreq(url, method, parameters):
+    req = oauth.Request.from_consumer_and_token(oauth_consumer,
+                                               token=oauth_token,
+                                               http_method=http_method,
+                                               http_url=url, 
+                                               parameters=parameters)
+
+    req.sign_request(signature_method_hmac_sha1, oauth_consumer, oauth_token)
+
+    headers = req.to_header()
+
+    if http_method == "POST":
+        encoded_post_data = req.to_postdata()
+    else:
+        encoded_post_data = None
+        url = req.to_url()
+
+    opener = urllib.OpenerDirector()
+    opener.add_handler(http_handler)
+    opener.add_handler(https_handler)
+
+    response = opener.open(url, encoded_post_data)
+
+    return response
+
+def fetchsamples():
+    url = "https://stream.twitter.com/1.1/statuses/filter.json?track=fitness,training,workout,workouts,running,crossfit&locations=-129.99,25.17,-66.62,49.15"
+    parameters = []
+    response = twitterreq(url, "POST", parameters)
+    for line in response:
+      print (line.strip())
+
+if __name__ == '__main__':
+    fetchsamples()
